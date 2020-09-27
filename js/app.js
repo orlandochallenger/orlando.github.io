eb0d7b11ff74440f2f9320c21c184777ddbe803d
orlandofat <orlandofat@gmail.com>
2020-09-27 12:51:20 -0400

    js scrol

--- js/app.js
+++ js/app.js
@@ -1,32 +1,20 @@
-var myNav = document.querySelector(".navbar");
+const myNav = document.querySelector(".navbar");
+const mybutton = document.getElementById("myBtn");
+
 window.onscroll = () => {
-  if (document.documentElement.scrollTop > 0) {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     myNav.classList.add("bg-dark");
     myNav.classList.remove("bg-info");
+    mybutton.style.display = "block";
   } else {
     myNav.classList.add("bg-info");
     myNav.classList.remove("bg-dark");
+    mybutton.style.display = "none";
   }
 };
 
-// //Get the button:
-// mybutton = document.getElementById("myBtn");
-
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = () => {
-//   scrollFunction()
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+// When the user clicks on the button, scroll to the top of the document
+topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
